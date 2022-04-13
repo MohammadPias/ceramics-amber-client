6bfcd45f6c219ca384acb662f1fdaed0975bd9cc
mohammad pias <mrpeyash@gmail.com>
2022-04-13 22:37:00 +0600

    product section updated

--- src/components/Home/Subscribe/Subscribe.js
+++ src/components/Home/Subscribe/Subscribe.js
@@ -5,11 +5,14 @@ const Subscribe = () => {
     const { register, handleSubmit } = useForm();
     const onSubmit = data => console.log(data);
     return (
-        <div className='mt-5 form-container position-relative'>
+        <div className='mt-5 form-container position-relative vh-50'>
+            <div className="heading-light">
+                <h2><span>Stay Tuned</span> With Us</h2>
+            </div>
             <div className='position-absolute top-100 start-50 translate-middle card-bg p-4 border shadow-sm rounded-3'>
-                <h4 className='text-center fw-bold w-75 ms-5'>Stay Connected With Our Email Updates</h4>
+                <h4 className='text-center fw-bold w-75 ms-5'>Subscribe</h4>
                 <form onSubmit={handleSubmit(onSubmit)}>
-                    <input placeholder='Your E-mail' {...register("email")} />
+                    <input className='text-center' placeholder='Your E-mail' {...register("email")} />
                     <input type="submit" />
                 </form>
             </div>
--- src/components/Home/TopProducts/Product.js
+++ src/components/Home/TopProducts/Product.js
@@ -1,18 +1,37 @@
 import React from 'react';
 import { Card, Col } from 'react-bootstrap';
+import Rating from "react-rating";
 
 const Product = ({ product }) => {
     const { title, img, price, rating } = product;
     return (
         <Col>
-            <Card className='h-100'>
+            <Card className='h-100 product-card shadow-sm'>
                 <Card.Img variant="top" src={img} />
+                <div className='img-cover'></div>
                 <Card.Body>
                     <Card.Title>{title}</Card.Title>
                     <Card.Text>
-                        <div className='product-rating'>
-
+                        <div>
+                            <Rating
+                                readonly
+                                initialRating={rating}
+                                emptySymbol="fa fa-star-o product-rating"
+                                fullSymbol="fa fa-star product-rating "
+                            />
                         </div>
+                        <div>
+                            <h5>Price: ${price}</h5>
+                        </div>
+                        <div className='hidden-btn'>
+                            <div className='cart-btn'>
+                                <i className="fa-solid fa-cart-shopping"></i>
+                            </div>
+                            <div className='heart-btn'>
+                                <i className="fa-solid fa-heart"></i>
+                            </div>
+                        </div>
+
                     </Card.Text>
                 </Card.Body>
             </Card>
