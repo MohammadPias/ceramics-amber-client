8249b43e2c3e9f99bb11e8f53ffc37623f4708d4
mohammad pias <mrpeyash@gmail.com>
2022-05-07 20:48:23 +0600

    update subscribe section

--- src/components/Home/Subscribe/Subscribe.js
+++ src/components/Home/Subscribe/Subscribe.js
@@ -9,8 +9,8 @@ const Subscribe = () => {
             <div className="heading-light">
                 <h2><span>Stay Tuned</span> With Us</h2>
             </div>
-            <div className='position-absolute top-100 start-50 translate-middle card-bg p-4 border shadow-sm rounded-3'>
-                <h4 className='text-center fw-bold w-75 ms-5'>Subscribe</h4>
+            <div className='w-50 position-absolute top-100 start-50 translate-middle card-bg p-4 border shadow-sm rounded-3'>
+                <h6 className='text-center fw-bold w-75 ms-5'>Subscribe us to get latest product update</h6>
                 <form onSubmit={handleSubmit(onSubmit)}>
                     <input className='text-center' placeholder='Your E-mail' {...register("email")} />
                     <input type="submit" />
